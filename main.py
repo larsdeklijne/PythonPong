f2faee96095357ef1f32c313941a72caf79a6e1b
1105799 <larsdeklijne@gmail.com>
2023-10-06 18:26:06 +0200

    alle spelers/ elementen gemaakt die ik nodig heb

--- main.py
+++ main.py
@@ -11,11 +11,26 @@ screen_height = 960
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Eerste PyGame: Pong')
 
+# rectangles voor de twee spelers
+ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15,30,30)
+player = pygame.Rect(screen_width - 20, screen_height/2 - 70,10,140)
+opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
+
+bg_color = pygame.Color('grey12')
+light_grey = (200,200,200)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    # rectangles plaatsen in scherm
+    screen.fill(bg_color)
+    pygame.draw.rect(screen, light_grey, player)
+    pygame.draw.rect(screen, light_grey, opponent)
+    pygame.draw.ellipse(screen, light_grey, ball)
+    pygame.draw.aaline(screen, light_grey, (screen_width/2,0), (screen_width/2,screen_height))
 
     #window update
     pygame.display.flip()
